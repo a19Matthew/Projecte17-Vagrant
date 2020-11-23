04f4bc4ee3e5684f3ad51cbd79d489162fff20d6
Mat <masiu@pop-os.localdomain>
2020-11-23 22:23:20 +0100

    0.5

--- provision-for-mysql.sh
+++ provision-for-mysql.sh
@@ -4,18 +4,18 @@ apt-get update
 apt-get install -y debconf-utils
 #Asigna root a una variable
 DB_ROOT_PASSWD=root
+#Regenerem el llenguatge
+locale-gen es_ES.UTF-8
 #Canvia la contrassenya de mysql
-debconf-set-selections <<< ”mysql-server mysql-server/root_password
-password $DB_ROOT_PASSWD”
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $DB_ROOT_PASSWD"
 #Envia un altre cop la contrassenya
-debconf-set-selections <<< ”mysql-server mysql-server/root_password_again
-password $DB_ROOT_PASSWD”
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DB_ROOT_PASSWD"
 #Instala mysql-server
 apt-get install -y mysql-server-5.5
-#Afegeix una linea de codi a mysqld.cnf
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+#Autoritza l'entrada des de qualsevol host
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mysql/my.cnf
 #Reinicia mysql
 /etc/init.d/mysql restart
 #Entra a mysql i dona tots els privilegis a root
-mysql -uroot mysql -p$DB_ROOT_PASSWD <<< ”GRANT ALL PRIVILEGES ON *.* TO
-root@'%' IDENTIFIED BY '$DB_ROOT_PASSWD'; FLUSH PRIVILEGES;”
+mysql -u root -p$DB_ROOT_PASSWD <<< "GRANT ALL PRIVILEGES ON *.* TO root@'%' IDENTIFIED BY '$DB_ROOT_PASSWD'; FLUSH PRIVILEGES;"
+mysql -u root -p$DB_ROOT_PASSWD <<< "CREATE DATABASE aso;"
